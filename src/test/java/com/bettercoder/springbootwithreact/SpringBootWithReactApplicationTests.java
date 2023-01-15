1e0ff448b53c780868cc6ac52a6d842ce75423f7
Kedar Nadkarny <kedar.nadkarny@gmail.com>
2023-01-14 20:16:56 -0500

    setup

---
+++ src/test/java/com/bettercoder/springbootwithreact/SpringBootWithReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bettercoder.springbootwithreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
